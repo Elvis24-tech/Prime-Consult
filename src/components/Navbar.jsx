7613e434bc2e88a43b32e899b35b1dbff53a1f55
elvis <kariuki.elvis@student.moringaschool.com>
2025-07-11 15:42:34 +0300

    wrote navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <h1>PrimeConsult</h1>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact Us</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
